3b1209a7024462c0a82a369e47dd83cc189e9768
quietdish <whale61@naver.com>
2026-05-06 13:59:00 +0900

    자바 시작, gitignore 추가

---
+++ src/be_study/start/HelloWorld.java
@@ -0,0 +1,29 @@
+package be_study.start;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, World!!");
+		
+		// 자바 코드 실행 : Ctrl + F11
+		
+		//주석 메모역할
+		
+		// 한줄 주석
+		/* 여러줄 주석
+		 * 엔터 치면서
+		 * 다음줄로 내려가도
+		 * 주석으로 인식 */
+		
+//		주석 : Ctrl + /
+//				Ctrl + Shift + C
+
+		// sysout 입력 -> Ctrl + Space 자동완성
+		//System.out.println();
+		System.out.println();		
+	}
+	
+
+}
