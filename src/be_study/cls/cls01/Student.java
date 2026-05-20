f33802e189c3f748322f529da273d872f82067f3
quietdish <whale61@naver.com>
2026-05-20 10:44:09 +0900

    클래스 객체 시작, 필드

---
+++ src/be_study/cls/cls01/Student.java
@@ -0,0 +1,24 @@
+package be_study.cls.cls01;
+
+public class Student {
+	
+	//학생 객체	->	클래스 (설계도 역할)
+	
+	//필드
+	// 이 클래스/객체/학생 이 가질 수 있는 or 상태나 특장을 표현할 수 있는
+	// 특징 속성
+	// 필드 변수
+	String name; //이름
+	int grade; //학년
+	String phone; //전화번호 연락처						int 놉??
+	String studentNumber; //학생의 학번	학생식별번호	13-34566 22k123
+	
+	
+	
+	//생성자
+//	println(name); xx
+	
+	
+	//메소드
+
+}
