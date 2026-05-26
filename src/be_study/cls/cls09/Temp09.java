0d8fb1f6fea8774e01f4963dd9081f797363139f
quietdish <whale61@naver.com>
2026-05-26 14:26:30 +0900

    클래스 활용(배열, 매개변수, 리턴타입)

---
+++ src/be_study/cls/cls09/Temp09.java
@@ -0,0 +1,16 @@
+package be_study.cls.cls09;
+
+public class Temp09 {
+
+	public int num;
+	
+	public void method() {
+		System.out.println("Temp09 method()");
+	}
+}
+
+/* main 내부 복붙.. 주석 ㅇㅅㅇ..
+객체 단위 활용
+Temp09 t1 = new Temp09();
+Temp09 t2 = new Temp09();
+Temp09 t3 = new Temp09(); */
