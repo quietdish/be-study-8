5a8ce1e7dbfdbf40a83a95d70ae9e28d5bc642ba
quietdish <whale61@naver.com>
2026-05-26 16:20:16 +0900

    클래스 리턴타입 활용 예시

---
+++ src/be_study/cls/cls11/ReturnMain.java
@@ -0,0 +1,57 @@
+package be_study.cls.cls11;
+
+public class ReturnMain {
+
+	public static void main(String[] args) {
+		
+		
+		//return
+		//return
+		//return
+		//return
+		
+		//데이터 전달을 위한 포장용도로 클래스
+		
+		
+		//성적
+		// 국어 수학 영어
+		AllScore score = getScore();
+		System.out.println(score.lang);
+		System.out.println(score.math);
+		System.out.println(score.eng);
+		
+	}
+	
+	public static AllScore getScore() {
+		AllScore as = new AllScore();
+		as.lang = 70;
+		as.math = 90;
+		as.eng = 80;
+		
+		return as;	//객체 1개 리턴
+	}
+	
+	
+	// 점수 3개를 한번에 전달	
+	//public static int ScoreInfo() {
+	public static String ScoreInfo() {	
+		// 70 80 90
+		
+		return 708090;	//국어 수학 영어
+		// 1009080
+		// 9010075
+		return 70,90,80;	//국어 수학 영어		
+		//return 은 한개...
+		//return "70|90|80";
+	}
+	
+	
+	public static int mathScore() {
+		return 90;
+	}
+	
+	public static int engScore() {
+		return 80;
+	}
+
+}
