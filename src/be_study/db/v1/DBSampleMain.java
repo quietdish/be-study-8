0055bf43d335b04e6f770f0c9581fc54155eae91
quietdish <whale61@naver.com>
2026-07-22 16:44:15 +0900

    DB 연동 select 조회 활용 - 1

---
+++ src/be_study/db/v1/DBSampleMain.java
@@ -0,0 +1,122 @@
+package be_study.db.v1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBSampleMain {
+
+	public static void main(String[] args) {
+
+		findDept();
+		
+		
+
+	}
+	
+	///메소드 ㅇㅅㅇ..
+	//	query select * from dept
+	public static void findDept() {
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {	
+			e.printStackTrace();
+		}
+		
+//		try {
+//		Class.forName("oracle.jdbc.driver.OracleDriver");
+//	} catch (Exception e) {
+//		// TODO: handle exception
+//	}
+		
+		//DB 연결 정보
+		
+		//String db_url = "jdbc:oracle:thin:@150.25.33.191:1521:orcl";
+		String db_url = "jdbc:oracle:thin:@localhost:1521:orcl";
+		String db_id = "scott";
+		String db_pw = "tiger";
+		
+		
+		//db 연결, 실행 객체
+		Connection conn = null;	//db 연결
+		PreparedStatement psmt = null;	//db 연결해서 sql 명령 실행해주는 객체
+		ResultSet rs = null;	//sql 실행 후 select 결과를 저장하는 객체
+		
+		
+		// DB 연결
+		
+		///안떠서 다 타이핑했음 ㅡ.ㅡ 빨간줄도 안뜨는데. 오타 안친것같은데...
+		try {
+			conn = DriverManager.getConnection(db_url, db_id, db_pw);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		//실행할 쿼리 준비
+		String sqlQuery = " select * from dept ";
+		
+		//쿼리 실행, 실행 후 후속 데이터 처리
+		
+		try {
+			
+			psmt = conn.prepareStatement(sqlQuery);
+			rs = psmt.executeQuery();
+			
+			while(rs.next()) {	//다음에 읽어올 데이터가 있는가? true 면 다음 데이터가 있다.
+				// rs 가 하나의 행을 가리키고, 열단위 값을 기준으로 조회
+								
+				// column Index 기준 조회
+//				System.out.print( rs.getInt(1) + " ");
+//				System.out.print( rs.getString(2) + " ");
+//				System.out.print( rs.getString(3) );
+				
+				// column 이름(별칭) 기준 조회
+				System.out.print( rs.getInt("deptno") + " ");
+				System.out.print( rs.getString("dname") + " ");
+				System.out.println( rs.getString("loc") );
+				
+				
+			}
+			
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		//DB 연결 종료
+		
+		try {
+			
+			if(rs != null)
+				rs.close();
+			if(rs != null)
+				rs.close();	//null 
+			if(rs != null)
+				psmt.close();
+			if(rs != null)
+				conn.close();
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
